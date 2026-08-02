99131d6d0554d135d8c732e7821cad210c95369e
JT365 <bruce.zhou2002@gmail.com>
2026-08-02 09:06:06 -0700

    modify drm_fbdev_generic_setup to drm_client_setup_on_probe

--- src/beada_drv.c
+++ src/beada_drv.c
@@ -27,7 +27,7 @@
 #include <drm/drm_probe_helper.h>
 #include <drm/drm_simple_kms_helper.h>
 #include <drm/drm_print.h>
-#include <drm/drm_fbdev_generic.h>
+#include <drm/drm_client_setup.h>
 
 #include "beada_device.h"
 
@@ -280,7 +280,10 @@ static int beada_usb_probe(struct usb_interface *interface,
 		goto err_put_device;
 	}
 
-	drm_fbdev_generic_setup(dev, 0);
+    ret = drm_client_setup_on_probe(dev, NULL, NULL);
+    if (ret)
+        dev_err(&beada->udev->dev,
+                  "drm_client_setup_on_probe() failed: %d\n", ret);
 
 	dev_info(&beada->udev->dev, "BeadaPanel %s detected\n", beada->geometrics.name);
 	return ret;
